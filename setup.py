31ffb7fca3fa0b7ea8a0f3648b1c9c1be7b8c38e
Kyle Monson <kyle@8minute.com>
2021-04-30 16:13:10 -0700

    Update version number.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     setup(
         name="koala2",
 
-        version="0.0.36+3",
+        version="0.0.36+4",
 
         author="Ants, open innovation lab",
         author_email="vallettea@gmail.com",
